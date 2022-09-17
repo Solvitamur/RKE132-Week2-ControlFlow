a1a0247efee84e8ea662686e5cb5a97446b2ab37
Solvita <solvitamur@gmail.com>
2022-09-18 01:09:17 +0300

    add week2 code

---
+++ AgeValidation/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Rakendus küsib kasutajal sisestada tema vanuse
+// Kui kasutaja vanus on väiksem kui 13, siis konsoolis kuvatakse:
+// "You are too young to use instagram"
+// muul juhul
+// konsoolis kuvatakse "welcome to instagram"
+
+Console.WriteLine("Enter your age:");
+
+string UserAge = Console.ReadLine();
+int UserAgeNum = 0;
+
+//boolean - true/false
+bool isAgeNumber = Int32.TryParse(UserAge, out UserAgeNum);
+Console.WriteLine($"Parse result {isAgeNumber}. User is {UserAgeNum} years old.");
+
+if(isAgeNumber)
+{
+    if (UserAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to instagram!");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use Instagram.");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age.");
+}
+
+
+
+
+//int UserAge = Int32.Parse(Console.ReadLine()); // "13" - heap, 13 - stack
+
+//if(UserAge >= 13)
+//{
+//    Console.WriteLine("Welcome to instagram");
+//}
+//else //if (userage < 13)
+//{
+//    Console.WriteLine("You are too young to use Instagram");
+//}
---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajal valida tema sugu (m/f)
+//rakendus küsib kasutajal tema perekonnanime
+//Lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+//"Welcome, Mr./Ms. [Kasutaja perekonnanimi]"
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+char UserGender = Char.Parse(Console.ReadLine()); //loeb konsoolist maha andmeid string (söne) formaadis
+
+Console.WriteLine("Please, enter your last name:");
+string UserLastName = Console.ReadLine();
+
+if (UserGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {UserLastName}!");
+}
+else if(UserGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {UserLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome {UserLastName}!");
+}
+
+
